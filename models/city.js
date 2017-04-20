d99d0f05d2ce383c0201d20b91847418463393c7
Muhammad Orabi <muhammad.orabi.2014@hotmail.com>
2017-04-20 22:53:03 +0200

    First Commit

---
+++ models/city.js
@@ -0,0 +1,9 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var city = new Schema({
+	name: String,
+	cal: [Schema.Types.Mixed]
+});
+
+module.exports = mongoose.model('City', city);
---
+++ routes/index.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var router = express.Router();
+var City = require('../models/city');
+
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.post('/city', function(req, res, next) {
+	var city = new City({ name: req.body.name, cal: req.body.cal });
+	city.save();
+	res.json(city);
+});
+
+router.get('/city/:name', function(req, res, next) {
+	City.findOne({'name': req.params.name}, function(err, city) {
+		if (err) { return new Error(); }
+		res.json(city);
+	});
+});
+
+module.exports = router;
