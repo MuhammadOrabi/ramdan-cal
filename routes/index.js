d038854090e8eb01a4a38ca89ce94f641d1888c6
Muhammad Orabi <muhammad.orabi.2014@hotmail.com>
2017-04-26 22:54:34 +0200

    Updates routes index.js

--- routes/index.js
+++ routes/index.js
@@ -16,62 +16,68 @@ router.post('/city', function(req, res, next) {
 });
 
 router.post('/city/data', function(req, res, next) {
-	var hash = req.body.mention.hashtags;
-	var name;
-	for (var i = 0; i < hash.length; i++) {
-		if (hash[i].hashtag) {
-			name = hash[i].hashtag;
+	City.find({}, 'name' ,function(err, names) {
+		if (!names) { return res.json(); }
+		names = _.pluck(names, 'name');
+		var hash = req.body.mention.hashtags;
+		var name;
+		for (var i = 0; i < hash.length; i++) {
+			if (hash[i].hashtag && _.contains(names, hash[i].hashtag)) {
+				name = hash[i].hashtag;
+			}
 		}
-	}
-	if (!name) { return res.status(422).json(); }
-	// if (name === 'مكة' || name === 'مكه' || name === 'مكة_المكرمة' || name === 'مكه_المكرمه') {
-	// 	name = 'مكة المكرمة';
-	// } else if (name === 'جده') {
-	// 	name = 'جدة';
-	// } else if (name === 'جيزان') {
-	// 	name = 'جازان';
-	// } else if (name === 'ابها') {
-	// 	name = 'أبها';
-	// } else if (name === 'الباحه') {
-	// 	name = 'الباحة';
-	// } else if (name === 'المدينة' || name === 'المدينه' || name === 'المدينة_المنورة' || name === 'المدينه_المنوره') {
-	// 	name = 'المدينة المنورة';
-	// }
+		City.findOne({'name': name}, function(err, city) {
+			if (err) { return res.status(500).json({ err: err }); }
+			if (!city) { return res.status(422).json(); }
 
-	City.findOne({'name': name}, function(err, city) {
-		if (err) { return res.status(500).json({ err: err }); }
-		if (!city) { return res.status(404).json('Not Found'); }
-
-		var d = new Date();
-		var n = d.getDate();
-		var tod_data = _.pluck(city.cal, n.toString());
-		var tom = n === 30 ? 1 : n + 1;
-		var tom_data = _.pluck(city.cal, tom.toString());
-		var data = {
-			      	"city": city.name,
-			      	"date": '' + n,
-			      	"Fajr": tod_data[0].Fajr,
-			      	"Sunrise": tod_data[0].Sunrise,
-			      	"Dhuhr": tod_data[0].Dhuhr,
-			      	"Asr": tod_data[0].Asr,
-			      	"Sunset": tod_data[0].Sunset,
-			      	"Maghrib": tod_data[0].Maghrib,
-			      	"Isha": tod_data[0].Isha,
-			      	"Imsak": tom_data[0].Imsak,
-			      	"Midnight": tom_data[0].Midnight,
-			      	"date-tomorrow": '' + tom,
-			      	"Fajr-tomorrow": tom_data[0].Fajr,
-			      	"Sunrise-tomorrow": tom_data[0].Sunrise,
-			      	"Dhuhr-tomorrow": tom_data[0].Dhuhr,
-			      	"Asr-tomorrow": tom_data[0].Asr,
-			      	"Sunset-tomorrow": tom_data[0].Sunset,
-			      	"Maghrib-tomorrow": tom_data[0].Maghrib,
-			      	"Isha-tomorrow": tom_data[0].Isha,
-			      	"Imsak-tomorrow": tom_data[0].Imsak,
-			      	"Midnight-tomorrow": tom_data[0].Midnight
-				};
-		res.json(data);
+			var d = new Date();
+			var n = d.getDate();
+			var tod_data = _.pluck(city.cal, n.toString());
+			var tom = n === 30 ? 1 : n + 1;
+			var tom_data = _.pluck(city.cal, tom.toString());
+			var data = {
+				      	"city": city.name,
+				      	"date": '' + n,
+				      	"Fajr": tod_data[0].Fajr,
+				      	"Sunrise": tod_data[0].Sunrise,
+				      	"Dhuhr": tod_data[0].Dhuhr,
+				      	"Asr": tod_data[0].Asr,
+				      	"Sunset": tod_data[0].Sunset,
+				      	"Maghrib": tod_data[0].Maghrib,
+				      	"Isha": tod_data[0].Isha,
+				      	"Imsak": tom_data[0].Imsak,
+				      	"Midnight": tom_data[0].Midnight,
+				      	"date-tomorrow": '' + tom,
+				      	"Fajr-tomorrow": tom_data[0].Fajr,
+				      	"Sunrise-tomorrow": tom_data[0].Sunrise,
+				      	"Dhuhr-tomorrow": tom_data[0].Dhuhr,
+				      	"Asr-tomorrow": tom_data[0].Asr,
+				      	"Sunset-tomorrow": tom_data[0].Sunset,
+				      	"Maghrib-tomorrow": tom_data[0].Maghrib,
+				      	"Isha-tomorrow": tom_data[0].Isha,
+				      	"Imsak-tomorrow": tom_data[0].Imsak,
+				      	"Midnight-tomorrow": tom_data[0].Midnight
+					};
+			res.json(data);
+		});
 	});
+
+	// if (!name) { return res.status(422).json(); }
+	// // if (name === 'مكة' || name === 'مكه' || name === 'مكة_المكرمة' || name === 'مكه_المكرمه') {
+	// // 	name = 'مكة المكرمة';
+	// // } else if (name === 'جده') {
+	// // 	name = 'جدة';
+	// // } else if (name === 'جيزان') {
+	// // 	name = 'جازان';
+	// // } else if (name === 'ابها') {
+	// // 	name = 'أبها';
+	// // } else if (name === 'الباحه') {
+	// // 	name = 'الباحة';
+	// // } else if (name === 'المدينة' || name === 'المدينه' || name === 'المدينة_المنورة' || name === 'المدينه_المنوره') {
+	// // 	name = 'المدينة المنورة';
+	// // }
+
+
 });
 // router.get('/city/:name/:no', function(req, res, next) {
 // 	City.findOne({'name': req.params.name}, function(err, city) {
