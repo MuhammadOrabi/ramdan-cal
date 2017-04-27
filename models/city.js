69cf68229b8f7f009610f76197540cff4fbb40c0
Muhammad Orabi <muhammad.orabi.2014@hotmail.com>
2017-04-27 11:29:47 +0200

    Final Updates

--- models/city.js
+++ models/city.js
@@ -3,7 +3,7 @@ var Schema = mongoose.Schema;
 
 var city = new Schema({
 	name: {type: String, unique: true},
-	cal: [Schema.Types.Mixed]
+	cal: Schema.Types.Mixed
 });
 
 module.exports = mongoose.model('City', city);
--- routes/index.js
+++ routes/index.js
@@ -7,14 +7,14 @@ var _ = require('underscore');
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
-
+// Create a new City
 router.post('/city', function(req, res, next) {
 	City.create({ name: req.body.name, cal: req.body.cal }, function (err, city) {
 	  	if (err) return res.status(500).json({ err: err });
 		res.json(city);
 	});
 });
-
+// Get the Data for Flowics API
 router.post('/city/data', function(req, res, next) {
 	City.find({}, 'name' ,function(err, names) {
 		if (!names) { return res.json(); }
@@ -22,73 +22,65 @@ router.post('/city/data', function(req, res, next) {
 		var hash = req.body.mention.hashtags;
 		var name;
 		for (var i = 0; i < hash.length; i++) {
-			if (hash[i].hashtag && _.contains(names, hash[i].hashtag)) {
-				name = hash[i].hashtag;
-				break;
+			if (hash[i].hashtag) {
+				var tmp = hash[i].hashtag;
+				if (tmp === 'مكة' || tmp === 'مكه' || tmp === 'مكة_المكرمة' || tmp === 'مكه_المكرمه') {
+					tmp = 'مكة المكرمة';
+				} else if (tmp === 'جده') {
+					tmp = 'جدة';
+				} else if (tmp === 'جيزان') {
+					tmp = 'جازان';
+				} else if (tmp === 'ابها') {
+					tmp = 'أبها';
+				} else if (tmp === 'الباحه') {
+					tmp = 'الباحة';
+				} else if (tmp === 'المدينة' || tmp === 'المدينه' || tmp === 'المدينة_المنورة' || tmp === 'المدينه_المنوره') {
+					tmp = 'المدينة المنورة';
+				}
+				if (_.contains(names, tmp)) {
+					name = tmp;
+					break;
+				}
 			}
 		}
+
 		City.findOne({'name': name}, function(err, city) {
 			if (err) { return res.status(500).json({ err: err }); }
 			if (!city) { return res.status(422).json(); }
 
 			var d = new Date();
 			var n = d.getDate();
-			var tod_data = _.pluck(city.cal, n.toString());
-			var tom = n === 30 ? 1 : n + 1;
-			var tom_data = _.pluck(city.cal, tom.toString());
+			var tod_data = city.cal[n.toString()];
+			var tom = n === 31 ? 1 : n + 1;
+			var tom_data = city.cal[tom.toString()];
 			var data = {
 				      	"city": city.name,
-				      	"date": '' + n,
-				      	"Fajr": tod_data[0].Fajr,
-				      	"Sunrise": tod_data[0].Sunrise,
-				      	"Dhuhr": tod_data[0].Dhuhr,
-				      	"Asr": tod_data[0].Asr,
-				      	"Sunset": tod_data[0].Sunset,
-				      	"Maghrib": tod_data[0].Maghrib,
-				      	"Isha": tod_data[0].Isha,
-				      	"Imsak": tom_data[0].Imsak,
-				      	"Midnight": tom_data[0].Midnight,
-				      	"date-tomorrow": '' + tom,
-				      	"Fajr-tomorrow": tom_data[0].Fajr,
-				      	"Sunrise-tomorrow": tom_data[0].Sunrise,
-				      	"Dhuhr-tomorrow": tom_data[0].Dhuhr,
-				      	"Asr-tomorrow": tom_data[0].Asr,
-				      	"Sunset-tomorrow": tom_data[0].Sunset,
-				      	"Maghrib-tomorrow": tom_data[0].Maghrib,
-				      	"Isha-tomorrow": tom_data[0].Isha,
-				      	"Imsak-tomorrow": tom_data[0].Imsak,
-				      	"Midnight-tomorrow": tom_data[0].Midnight
+				      	"date": tod_data.day,
+				      	"Fajr": tod_data.Fajr,
+				      	"Sunrise": tod_data.Sunrise,
+				      	"Dhuhr": tod_data.Dhuhr,
+				      	"Asr": tod_data.Asr,
+				      	"Sunset": tod_data.Sunset,
+				      	"Maghrib": tod_data.Maghrib,
+				      	"Isha": tod_data.Isha,
+				      	"Imsak": tom_data.Imsak,
+				      	"Midnight": tom_data.Midnight,
+				      	"date-tomorrow": tom_data.day,
+				      	"Fajr-tomorrow": tom_data.Fajr,
+				      	"Sunrise-tomorrow": tom_data.Sunrise,
+				      	"Dhuhr-tomorrow": tom_data.Dhuhr,
+				      	"Asr-tomorrow": tom_data.Asr,
+				      	"Sunset-tomorrow": tom_data.Sunset,
+				      	"Maghrib-tomorrow": tom_data.Maghrib,
+				      	"Isha-tomorrow": tom_data.Isha,
+				      	"Imsak-tomorrow": tom_data.Imsak,
+				      	"Midnight-tomorrow": tom_data.Midnight
 					};
 			res.json(data);
 		});
 	});
-
-	// if (!name) { return res.status(422).json(); }
-	// // if (name === 'مكة' || name === 'مكه' || name === 'مكة_المكرمة' || name === 'مكه_المكرمه') {
-	// // 	name = 'مكة المكرمة';
-	// // } else if (name === 'جده') {
-	// // 	name = 'جدة';
-	// // } else if (name === 'جيزان') {
-	// // 	name = 'جازان';
-	// // } else if (name === 'ابها') {
-	// // 	name = 'أبها';
-	// // } else if (name === 'الباحه') {
-	// // 	name = 'الباحة';
-	// // } else if (name === 'المدينة' || name === 'المدينه' || name === 'المدينة_المنورة' || name === 'المدينه_المنوره') {
-	// // 	name = 'المدينة المنورة';
-	// // }
-
-
 });
-// router.get('/city/:name/:no', function(req, res, next) {
-// 	City.findOne({'name': req.params.name}, function(err, city) {
-// 		if (err) { return res.status(500).json({ err: err }); }
-// 		var no = req.params.no;
-// 		var data = _.pluck(city.cal, no);
-// 		res.json(data);
-// 	});
-// });
-
+// Get the output Schema for Flowics
 router.get('/schema/city', function(req, res, next) {
 	var schema = {
 		  "type":"object",
@@ -210,5 +202,31 @@ router.get('/schema/city', function(req, res, next) {
 		};
 	res.json(schema);
 });
+
+// Get All the data
+router.get('/city/all', function(req, res, next) {
+	City.find({}, function(err, all) {
+		if (err) { return res.status(500).json({ err: err }); }
+		if (!all) { return res.status(404).json('Not Found'); }
+		res.json(all);
+	});
+});
+
+// Update City By Name
+router.put('/city/:name', function(req, res, next) {
+	City.findOneAndUpdate({ name: req.params.name }, { cal: req.body.cal }, function(err, city) {
+		if (err) { return res.status(500).json({ err: err }); }
+		if (!city) { return res.status(404).json('Not Found'); }
+		res.json(city);
+	});
+});
+
+// Delete City By Name
+router.delete('/city/:name', function(req, res, next) {
+	City.remove({ name: req.params.name }, function(err) {
+		if (err) { return res.status(500).json({ err: err }); }
+		res.status(404).json('Deleted Succesfully');
+	});
+})
 
 module.exports = router;
